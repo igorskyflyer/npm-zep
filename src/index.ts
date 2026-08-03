f7825b989b60d02c59d4dab8809de8538c7acbad
igorskyflyer <igor.dvlpr@gmail.com>
2026-08-04 01:52:53 +0200

    🔨 logic: update with v7 API

--- src/index.ts
+++ src/index.ts
@@ -1,140 +1,341 @@
 // Author: Igor Dimitrijević (@igorskyflyer)
 
-export type ZepCallback = (...args: any[]) => void
-export type ZepErrorHandler = (error: unknown) => void
-export type ZepEventHandler = () => void
+export type ZepCallback<T extends unknown[]> = (...args: T) => void;
+export type ZepErrorHandler = (error: unknown) => void;
+export type ZepEventHandler = () => void;
 
-export class Zep {
-  #timer: number | NodeJS.Timeout
-  #shouldCancel: boolean
-  #shouldAbort: boolean
-  #calls: number
-  #isRunning: boolean
-  #time: number | undefined
-  #executionCount: number
-  #isWaiting: boolean
-  #wasCancelled: boolean
-  #wasAborted: boolean
-  #callback: ZepCallback
-  #onCancelled?: ZepEventHandler
-  #onAborted?: ZepEventHandler
-  #onBeforeRun?: ZepEventHandler
-  #onAfterRun?: ZepEventHandler
-  #onCompleted?: ZepEventHandler
-  #onError?: ZepErrorHandler
+const ZEP_MAX_TIME = 2_147_483_647; // 32-bit signed int max
+const ZEP_DEFAULT_TIME = 150;
+const ZEP_EMPTY_HANDLER = () => {};
+
+export class Zep<T extends unknown[] = unknown[]> {
+  #timer: ReturnType<typeof setTimeout> | undefined;
+  #args: T | undefined;
+  #shouldCancel: boolean;
+  #lastCalled: number;
+  #invocations: number;
+  #isRunning: boolean;
+  #time: number;
+  #executionCount: number;
+  #isWaiting: boolean;
+  #wasCancelled: boolean;
+  #wasAborted: boolean;
+  #cancelledCount: number;
+  #abortedCount: number;
+  #maxBurstCalls: number;
+  #currentBurstCalls: number;
+  #callback: ZepCallback<T>;
+  #onCancelled: ZepEventHandler;
+  #onAborted: ZepEventHandler;
+  #onBeforeRun: ZepEventHandler;
+  #onAfterRun: ZepEventHandler;
+  #onCompleted: ZepEventHandler;
+  #onError: ZepErrorHandler;
 
   /**
    * Creates a new instance of Zep.
    * @param callback The function/callback to debounce.
-   * @param time The time limit (in ms) for the debouncing.
+   * @param time The time limit (in ms) for the debouncing. Defaults to 150ms.
+   * @throws Throws if any of the parameters is not valid.
    */
-  constructor(callback: ZepCallback, time?: number) {
-    this.#timer = 0
-    this.#shouldCancel = false
-    this.#shouldAbort = false
-    this.#calls = 0
-    this.#isRunning = false
-    this.#callback = callback
-    this.#time = time
-    this.#executionCount = 0
-    this.#isWaiting = false
-    this.#wasCancelled = false
-    this.#wasAborted = false
+  constructor(callback: ZepCallback<T>, time: number = ZEP_DEFAULT_TIME) {
+    if (typeof callback !== 'function') {
+      throw new TypeError('Callback must be a function.');
+    }
 
-    this.#onCancelled = undefined
-    this.#onAborted = undefined
-    this.#onBeforeRun = undefined
-    this.#onAfterRun = undefined
-    this.#onCompleted = undefined
-    this.#onError = undefined
+    if (typeof time !== 'number' || Number.isNaN(time)) {
+      throw new TypeError('Time must be a number.');
+    }
+
+    if (time < 0 || time > ZEP_MAX_TIME) {
+      throw new RangeError(`Time must be >= 0 and <= ${ZEP_MAX_TIME}ms.`);
+    }
+
+    this.#timer = undefined;
+    this.#lastCalled = 0;
+    this.#shouldCancel = false;
+    this.#invocations = 0;
+    this.#isRunning = false;
+    this.#callback = callback;
+    this.#time = time;
+    this.#executionCount = 0;
+    this.#isWaiting = false;
+    this.#wasCancelled = false;
+    this.#wasAborted = false;
+    this.#cancelledCount = 0;
+    this.#abortedCount = 0;
+    this.#maxBurstCalls = 0;
+    this.#currentBurstCalls = 0;
+
+    this.#onCancelled = ZEP_EMPTY_HANDLER;
+    this.#onAborted = ZEP_EMPTY_HANDLER;
+    this.#onBeforeRun = ZEP_EMPTY_HANDLER;
+    this.#onAfterRun = ZEP_EMPTY_HANDLER;
+    this.#onCompleted = ZEP_EMPTY_HANDLER;
+    this.#onError = ZEP_EMPTY_HANDLER;
   }
 
   #deleteTimer() {
-    clearInterval(this.#timer)
-    this.#timer = 0
+    clearTimeout(this.#timer);
+
+    this.#timer = undefined;
+    this.#isRunning = false;
+    this.#isWaiting = false;
+    this.#shouldCancel = false;
+  }
+
+  #clearArgs() {
+    this.#args = undefined;
+  }
+
+  #schedule(delay: number) {
+    this.#timer = setTimeout(() => {
+      if (this.#shouldCancel) {
+        this.#wasCancelled = true;
+        this.#cancelledCount++;
+
+        this.#deleteTimer();
+        this.#clearArgs();
+        this.#onCancelled();
+
+        return;
+      }
+
+      const elapsed = performance.now() - this.#lastCalled;
+
+      if (elapsed < this.#time) {
+        this.#schedule(Math.max(1, this.#time - elapsed));
+        return;
+      }
+
+      this.#deleteTimer();
+      this.#isWaiting = false;
+      this.#execute();
+    }, delay);
+  }
+
+  #execute() {
+    /* v8 ignore next 3 */
+    if (this.#args === undefined) {
+      return;
+    }
+
+    this.#onBeforeRun();
+
+    this.#isRunning = true;
+
+    try {
+      this.#callback(...this.#args);
+    } catch (e) {
+      if (this.#onError === ZEP_EMPTY_HANDLER) {
+        queueMicrotask(() => {
+          throw e;
+        });
+      } else {
+        this.#onError(e);
+      }
+    } finally {
+      this.#isRunning = false;
+      this.#executionCount++;
+
+      if (!this.#isWaiting) {
+        this.#currentBurstCalls = 0;
+        this.#clearArgs();
+      }
+    }
+
+    this.#onAfterRun();
+    this.#onCompleted();
   }
 
   /**
    * Returns the number of callback executions.
    */
   get executionCount(): number {
-    return this.#executionCount
+    return this.#executionCount;
   }
 
   /**
-   * Indicates whether Zep is waiting for a Timer to finish its execution, if true, Zep.run() won’t create new Timers when called.
+   * Indicates whether Zep is waiting for a Timer to finish its execution.
+   *
+   * If true, Zep.run() won’t create new Timers when called.
    */
   get isWaiting(): boolean {
-    return this.#isWaiting
+    return this.#isWaiting;
   }
 
   /**
    * Indicates whether a Timer is currently running the `callback` provided in the constructor.
    */
   get isRunning(): boolean {
-    return this.#isRunning
+    return this.#isRunning;
   }
 
   /**
-   * Indicates whether the execution of Zep.run() was cancelled. Execution can be cancelled by calling Zep.cancel().
+   * Indicates whether the execution of Zep.run() was cancelled.
+   *
+   * Execution can be cancelled by calling Zep.cancel().
    */
   get wasCancelled(): boolean {
-    return this.#wasCancelled
+    return this.#wasCancelled;
   }
 
   /**
-   * Indicates whether the execution of Zep.run() was aborted. Execution can be aborted by calling Zep.abort().
+   * Indicates whether the execution of Zep.run() was aborted.
+   *
+   * Execution can be aborted by calling Zep.abort().
    */
   get wasAborted(): boolean {
-    return this.#wasAborted
+    return this.#wasAborted;
+  }
+
+  /**
+   * Returns the number of callback invocations.
+   */
+  get invocations(): number {
+    return this.#invocations;
+  }
+
+  /**
+   * Returns the number of cancelled executions.
+   */
+  get cancelledCount(): number {
+    return this.#cancelledCount;
+  }
+
+  /**
+   * Returns the number of aborted executions.
+   */
+  get abortedCount(): number {
+    return this.#abortedCount;
+  }
+
+  /**
+   * Returns the number of maximum burst calls.
+   */
+  get maxBurstCalls(): number {
+    return this.#maxBurstCalls;
+  }
+
+  /**
+   * Returns the number of current burst calls.
+   */
+  get currentBurstCalls(): number {
+    return this.#currentBurstCalls;
   }
 
   /**
    * A handler to call when the execution of Zep.run() has been cancelled.
    */
-  onCancelled(handler: ZepEventHandler): Zep {
-    this.#onCancelled = handler
-    return this
+  onCancelled(handler: ZepEventHandler): Zep<T> {
+    if (typeof handler === 'function') {
+      this.#onCancelled = handler;
+    } else {
+      this.#onCancelled = ZEP_EMPTY_HANDLER;
+    }
+
+    return this;
   }
 
   /**
    * A handler to call when the execution of Zep.run() has been aborted.
    */
-  onAborted(handler: ZepEventHandler): Zep {
-    this.#onAborted = handler
-    return this
+  onAborted(handler: ZepEventHandler): Zep<T> {
+    if (typeof handler === 'function') {
+      this.#onAborted = handler;
+    } else {
+      this.#onAborted = ZEP_EMPTY_HANDLER;
+    }
+
+    return this;
   }
 
   /**
    * A handler to call before Zep.run().
    */
-  onBeforeRun(handler: ZepEventHandler): Zep {
-    this.#onBeforeRun = handler
-    return this
+  onBeforeRun(handler: ZepEventHandler): Zep<T> {
+    if (typeof handler === 'function') {
+      this.#onBeforeRun = handler;
+    } else {
+      this.#onBeforeRun = ZEP_EMPTY_HANDLER;
+    }
+
+    return this;
   }
 
   /**
    * A handler to call after Zep.run().
    */
-  onAfterRun(handler: ZepEventHandler): Zep {
-    this.#onAfterRun = handler
-    return this
+  onAfterRun(handler: ZepEventHandler): Zep<T> {
+    if (typeof handler === 'function') {
+      this.#onAfterRun = handler;
+    } else {
+      this.#onAfterRun = ZEP_EMPTY_HANDLER;
+    }
+
+    return this;
   }
 
   /**
    * A handler to call after `Zep()` has finished running, i.e. no more calls to the `Zep.run()` method have been issued in the given time-frame.
    */
-  onCompleted(handler: ZepEventHandler): Zep {
-    this.#onCompleted = handler
-    return this
+  onCompleted(handler: ZepEventHandler): Zep<T> {
+    if (typeof handler === 'function') {
+      this.#onCompleted = handler;
+    } else {
+      this.#onCompleted = ZEP_EMPTY_HANDLER;
+    }
+
+    return this;
   }
 
   /**
    * A handler to call when an error has occurred during execution.
    */
-  onError(handler: ZepErrorHandler): Zep {
-    this.#onError = handler
-    return this
+  onError(handler: ZepErrorHandler): Zep<T> {
+    if (typeof handler === 'function') {
+      this.#onError = handler;
+    } else {
+      this.#onError = ZEP_EMPTY_HANDLER;
+    }
+
+    return this;
+  }
+
+  /**
+   * Automatically releases all resources, clears active timers,
+   * and unbinds handlers to prevent memory leaks when the instance
+   * goes out of scope. Supports the explicit resource management
+   * (`using` declaration) to clean up instances automatically
+   * when exiting scope.
+   */
+  [Symbol.dispose](): void {
+    this.#deleteTimer();
+    this.#clearArgs();
+
+    this.#onCancelled = ZEP_EMPTY_HANDLER;
+    this.#onAborted = ZEP_EMPTY_HANDLER;
+    this.#onBeforeRun = ZEP_EMPTY_HANDLER;
+    this.#onAfterRun = ZEP_EMPTY_HANDLER;
+    this.#onCompleted = ZEP_EMPTY_HANDLER;
+    this.#onError = ZEP_EMPTY_HANDLER;
+  }
+
+  /**
+   * Resets all internal counters, flags, and metrics, and stops any active timer.
+   */
+  reset(): void {
+    this.#deleteTimer();
+    this.#clearArgs();
+
+    this.#invocations = 0;
+    this.#executionCount = 0;
+    this.#cancelledCount = 0;
+    this.#abortedCount = 0;
+    this.#maxBurstCalls = 0;
+    this.#currentBurstCalls = 0;
+    this.#wasCancelled = false;
+    this.#wasAborted = false;
   }
 
   /**
@@ -142,7 +343,9 @@ export class Zep {
    * @see abort
    */
   cancel(): void {
-    this.#shouldCancel = true
+    if (this.#timer) {
+      this.#shouldCancel = true;
+    }
   }
 
   /**
@@ -150,122 +353,93 @@ export class Zep {
    * @see cancel
    */
   abort(): void {
-    this.#shouldAbort = true
+    if (this.#timer) {
+      this.#deleteTimer();
+      this.#clearArgs();
+
+      this.#wasAborted = true;
+      this.#abortedCount++;
+
+      this.#onAborted();
+    }
   }
 
   /**
-   * Writes Zep statistical information to the console.
+   * Writes Zep's statistical information to the console, including invocations, executions, saved call percentage, active/peak burst metrics, and cancellation/abort counts.
    */
   writeStats(): void {
-    let percentageSaved: string
+    let percentageSaved: string;
 
-    // handles undefined and 0
-    if (this.#executionCount && this.#calls) {
+    if (this.#executionCount && this.#invocations) {
       percentageSaved = (
         100 -
-        (this.#executionCount / this.#calls) * 100
-      ).toFixed(2)
+        (this.#executionCount / this.#invocations) * 100
+      ).toFixed(2);
+    } else if (this.#invocations > 0 && this.#executionCount === 0) {
+      percentageSaved = '100.00';
     } else {
-      percentageSaved = '0'
+      percentageSaved = '0.00';
     }
 
     // biome-ignore lint/suspicious/noConsole: needed for DX
     console.log(
-      `🧠 [Zep]: invocations: ${this.#calls}, callback executions: ${this.#executionCount}, saving ${percentageSaved}% of calls.`
-    )
+      `🧠 [Zep Metrics Report]\n` +
+        `├── Invocations  : ${this.#invocations}\n` +
+        `├── Executions   : ${this.#executionCount} (${percentageSaved}% saved)\n` +
+        `├── Active Burst : ${this.#currentBurstCalls} pending\n` +
+        `├── Peak Burst   : ${this.#maxBurstCalls} max squashed\n` +
+        `└── Terminations : ${this.#cancelledCount} cancels, ${this.#abortedCount} aborts`,
+    );
   }
 
   /**
    * Runs the callback defined in the constructor if necessary or else debounces it.
    */
-  run(...args: any[]): Zep {
-    if (typeof this.#callback !== 'function') {
-      return this
+  run(...args: T): Zep<T> {
+    let justCancelled = false;
+
+    if (this.#shouldCancel) {
+      this.#wasCancelled = true;
+      this.#cancelledCount++;
+
+      this.#deleteTimer();
+      this.#clearArgs();
+      this.#onCancelled();
+
+      justCancelled = true;
     }
 
-    if (this.#shouldAbort) {
-      this.#deleteTimer()
-
-      this.#isRunning = false
-      this.#shouldAbort = false
-      this.#wasAborted = true
-
-      if (typeof this.#onAborted === 'function') {
-        this.#onAborted()
-      }
-
-      // don't let the execution continue!
-      return this
+    if (!(this.#isWaiting || this.#isRunning)) {
+      this.#currentBurstCalls = 0;
     }
 
-    this.#calls++
+    this.#args = args;
+    this.#invocations++;
+    this.#currentBurstCalls++;
 
-    this.#wasCancelled = false
-    this.#wasAborted = false
-
-    this.#isWaiting = true
-    this.#isRunning = true
-
-    if (!this.#time) {
-      try {
-        this.#callback(...args)
-      } catch (e) {
-        if (typeof this.#onError === 'function') {
-          this.#onError(e)
-        }
-      }
-
-      this.#executionCount++
-      this.#isWaiting = false
-      this.#isRunning = false
-      return this
+    if (this.#currentBurstCalls > this.#maxBurstCalls) {
+      this.#maxBurstCalls = this.#currentBurstCalls;
     }
+
+    if (!justCancelled) {
+      this.#wasCancelled = false;
+    }
+
+    this.#wasAborted = false;
+    this.#shouldCancel = false;
+
+    if (this.#time === 0) {
+      this.#execute();
+      return this;
+    }
+
+    this.#lastCalled = performance.now();
+    this.#isWaiting = true;
 
     if (!this.#timer) {
-      this.#timer = setInterval(() => {
-        if (!this.#isRunning) {
-          this.#deleteTimer()
-
-          this.#isWaiting = false
-          this.#isRunning = false
-
-          if (!this.#wasCancelled && typeof this.#onCompleted === 'function') {
-            this.#onCompleted()
-          }
-
-          return
-        }
-
-        if (this.#shouldCancel && typeof this.#onCancelled === 'function') {
-          this.#isRunning = false
-          this.#shouldCancel = false
-          this.#wasCancelled = true
-
-          this.#onCancelled.call(this)
-        }
-
-        if (typeof this.#onBeforeRun === 'function') {
-          this.#onBeforeRun()
-        }
-
-        try {
-          this.#callback(...args)
-        } catch (e) {
-          if (typeof this.#onError === 'function') {
-            this.#onError(e)
-          }
-        }
-
-        if (typeof this.#onAfterRun === 'function') {
-          this.#onAfterRun()
-        }
-
-        this.#executionCount++
-
-        this.#isWaiting = false
-        this.#isRunning = false
-      }, this.#time)
+      this.#schedule(this.#time);
     }
-    return this
+
+    return this;
   }
 }
